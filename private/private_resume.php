df5dd360d07e0416c32426db93f027034b45e677
hharouna <hharouna86@gmail.com>
2023-09-23 03:13:07 -0400

    delete file empty

--- private/private_resume.php
+++
@@ -1,6 +0,0 @@
-<?php 
- 
- $dbh = new PDO('mysql:host=localhost;dbname=resumehharouna', "root", "0000001LE@");
- //$dbh = new PDO('mysql:host=localhost;dbname=c1prendall', "root", "eydf-MxkhI@CDC!J");
-  
-?>
